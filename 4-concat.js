eb37736cd81c83f086ffff744b26a16554f62c15
Trusilla <lavinatrue@gmail.com>
2026-02-10 22:30:56 +0300

    Value of my arguments

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+// 4-concat.js
+
+// Get the first and second arguments from command line
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Print them in the required format
+console.log(`${arg1} is ${arg2}`);
+
